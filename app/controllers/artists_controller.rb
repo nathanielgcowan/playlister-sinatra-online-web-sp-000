6ac74ec5ed9185272bc4578fd822196674d5632e
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 18:39:02 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,8 +3,8 @@ class ArtistsController < ApplicationController
   get '/artists' do
   # #this should present the user with a list of all artists in the library.
   # #each artist should be a clickable link to that particular artist's show page
-  @artists = Artist.all
-  erb: 'artists/index'
+    @artists = Artist.all
+    erb: 'artists/index'
   end
 
 end
