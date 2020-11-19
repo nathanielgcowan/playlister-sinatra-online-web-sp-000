17597ad7903787dada3a8e7b285952bb2befb877
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 20:08:42 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_200657) do
+ActiveRecord::Schema.define(version: 2020_11_19_200758) do
+
+  create_table "genres", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "songs", force: :cascade do |t|
     t.string "name"
