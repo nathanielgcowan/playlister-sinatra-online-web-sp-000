4ce5ca18cb76a811d44fd4d12d8023329e0dfdd1
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 19:38:31 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -3,9 +3,9 @@ class Genre < ActiveRecord::Base
   has_many :songs, through: :song_genres
   has_many :artists, through: :songs
 
-  def slug 
+  def slug
     name.downcase.tr('', '-')
-  end 
+  end
 
   def self.find_by_slug(slug)
     Genre.all.find { |genre| genre.slug == slug }
