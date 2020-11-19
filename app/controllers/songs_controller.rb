57f44bb14f1edbbf0c6ab667914f344dd689d8bc
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-11-19 19:29:03 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,10 @@
 require 'rack-flash'
 
 class SongsController < ApplicationController
-  user Rack::Flash
+  use Rack::Flash
 
   get 'songs' do
     @songs = Songs.all
-
     erb:'/songs/index'
   end
 
